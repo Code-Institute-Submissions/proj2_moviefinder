cb1200575881606562a8b1e648e5d6d0850ca4db
edmundgjj <edmundgjj@gmail.com>
2020-12-27 19:40:14 +0000

    Added Search Function and Display Search Results

--- static/main.js
+++ static/main.js
@@ -1,23 +1,50 @@
 let movie_api = '57b5e64d'
 let search_url = `https://www.omdbapi.com/?apikey=${movie_api}&s=`
 let info_url = `https://www.omdbapi.com/?apikey=${movie_api}&i=`
-
-$("#submitMovieName").click(function () {
-
-    console.log("Clicked");
-    input = $("#movieNameInput").val();
-    console.log(input)
-});
-
-
+let search_input = '';
+let search_list = '';
+let search_results_data = '';
 
 function searchMovie(movie_name) {
     axios.get(search_url + movie_name)
-        .then(function(response){
-            let search_results = response.data.Search;
-            let search_list = '';
-
-        })
+        .then(function (response) {
+            search_results_data = response.data.Search;
+            console.log(search_results_data);
+        }).catch(function (err) {
+            console.log(err);
+        });
 }
 
+function createMovieCard(data) {
+    console.log('Create Movie');
+    search_list += `<div class="card">
+    <div class="card-header">${data.Title}</div>
+    <img class="card-img-top" src="${data.Poster}" alt="Movie Poster of ${data.Title}">
+    <div class="card-footer">
+    <a class="btn btn-primary" href="https://www.imdb.com/title/${data.imdbID}/" role="button" target="_blank">View Movie</a>
+    </div>
+    </div>`;
+
+    $("#cardList").html(search_list);
+};
+
+
+$("#submitMovieName").click(function () {
+    if ($("#movieNameInput").val()) {
+        $('#message').html("")
+        $('#demo').html("")
+        search_input = $("#movieNameInput").val();
+        searchMovie(search_input);
+        search_list = "";
+        for (var i = 0, len = search_results_data.length; i < len; i++) {
+            console.log(i)
+            createMovieCard(search_results_data[i]);
+        };
+    } else {
+        $('#message').html("Please enter a movie name.")
+        $('#demo').html("")
+    }
+
+    event.preventDefault();
+});
 
